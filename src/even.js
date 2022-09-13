10ff74be5b948a6f388fee73cd0cdb576613dda5
Alex Trava <aleksei.travnov@mail.ru>
2022-09-13 22:21:59 +0400

    update

--- src/even.js
+++ src/even.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import run from './index.js';
 
 const randomNumber = (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,29 +11,17 @@ const checkChet = (number) => {
   }
   return 'no';
 };
+const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+let rightAnswer = 'yes';
+const minRange = 1;
+const maxRange = 10;
 
-const startEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let rightAnswer = 'yes';
-  const minRange = 1;
-  const maxRange = 10;
-  for (let i = 0; i < 3; i += 1) {
-    const number = randomNumber(minRange, maxRange);
-    console.log(`Question:${number}`);
-    const answer = readlineSync.question('Your answer:');
-    rightAnswer = checkChet(number);
-    if (rightAnswer === answer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
-    }
-  }
+const start = () => {
+  const numberQuestion = randomNumber(minRange, maxRange);
+  rightAnswer = checkChet(numberQuestion);
+  return [numberQuestion, rightAnswer];
 };
 
-export default startEven;
+export default () => {
+  run(rule, start);
+};
