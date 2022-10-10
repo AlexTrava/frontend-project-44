d0e5c26ed2fe310b7ab4e4601fb3efd79b079f0b
Alex Trava <aleksei.travnov@mail.ru>
2022-10-10 22:13:34 +0400

    update even

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,15 @@
 import run from '../index.js';
 import randomNumber from '../random.js';
 
-const checkChet = (number) => {
-  if (number % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+const isEVen = (number) => number % 2 === 0;
+
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const minRange = 1;
 const maxRange = 10;
 
 const start = () => {
   const question = randomNumber(minRange, maxRange);
-  const rightAnswer = checkChet(question);
+  const rightAnswer = isEVen(question) ? 'yes' : 'no';
   return [question, rightAnswer];
 };
 
